664bf5f4e341c44582822b54e4e1f1433e7aced8
Mick McCarthy <mccarthy.mick0012@gmail.com>
2019-04-28 22:50:49 +0100

    Add basic vector operations

--- src/vector2.lua
+++ src/vector2.lua
@@ -7,4 +7,39 @@ function Vector.new(x, y)
     return v
 end
 
+function Vector.add(v1, v2)
+    local v = {}
+    v.x = v1.x + v2.x
+    v.y = v1.y + v2.y
+    return v
+end
+
+function Vector.subtract(v1, v2)
+    local v = {}
+    v.x = v1.x - v2.x
+    v.y = v1.y - v2.y
+    return v
+end
+
+function Vector.negate(v1)
+    local v = {}
+    v.x = -v1.x
+    v.y = -v1.y
+    return v
+end
+
+function Vector.multiply(v1, s1)
+    local v = {}
+    v.x = v1.x * s1
+    v.y = v1.y * s1
+    return v
+end
+
+function Vector.divide(v1, s1)
+    local v = {}
+    v.x = v1.x / s1
+    v.y = v1.y / s1
+    return v
+end
+
 return Vector
