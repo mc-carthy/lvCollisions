4f93eb10e047ba083bebcc8a7090c74ee62043ad
Mick McCarthy <mccarthy.mick0012@gmail.com>
2019-04-29 23:00:35 +0100

    Add circle-circle collision

--- src/circle.lua
+++ src/circle.lua
@@ -1,3 +1,5 @@
+local Vector = require('src.vector2')
+
 local Circle = {}
 
 Circle.__index = Circle
@@ -10,6 +12,12 @@ function Circle.new(centre, radius)
     c.centre = centre
     c.radius = radius
     return c
+end
+
+function Circle.circleCollision(c1, c2)
+    local radiusSum = c1.radius + c2.radius
+    local centreVector = c1.centre:subtract(c2.centre)
+    return centreVector:length() <= radiusSum
 end
 
 return Circle
