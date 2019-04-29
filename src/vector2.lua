30cd8e27bf1da4f8bb6ff13330d8c67d561833ad
Mick McCarthy <mccarthy.mick0012@gmail.com>
2019-04-29 23:02:43 +0100

    Vector functions should return a Vector object when possible

--- src/vector2.lua
+++ src/vector2.lua
@@ -10,38 +10,33 @@ function Vector.new(x, y)
 end
 
 function Vector.add(v1, v2)
-    local v = {}
-    v.x = v1.x + v2.x
-    v.y = v1.y + v2.y
-    return v
+    local x = v1.x + v2.x
+    local y = v1.y + v2.y
+    return Vector.new(x, y)
 end
 
 function Vector.subtract(v1, v2)
-    local v = {}
-    v.x = v1.x - v2.x
-    v.y = v1.y - v2.y
-    return v
+    local x = v1.x - v2.x
+    local y = v1.y - v2.y
+    return Vector.new(x, y)
 end
 
 function Vector.negate(v1)
-    local v = {}
-    v.x = -v1.x
-    v.y = -v1.y
-    return v
+    local x = -v1.x
+    local y = -v1.y
+    return Vector.new(x, y)
 end
 
 function Vector.multiply(v1, s1)
-    local v = {}
-    v.x = v1.x * s1
-    v.y = v1.y * s1
-    return v
+    local x = v1.x * s1
+    local y = v1.y * s1
+    return Vector.new(x, y)
 end
 
 function Vector.divide(v1, s1)
-    local v = {}
-    v.x = v1.x / s1
-    v.y = v1.y / s1
-    return v
+    local x = v1.x / s1
+    local y = v1.y / s1
+    return Vector.new(x ,y)
 end
 
 function Vector.length(v1)
@@ -57,10 +52,9 @@ function Vector.unit(v1)
 end
 
 function Vector.rotate(v1, rad)
-    local v = {}
-    v.x = v1.x * math.cos(rad) - v1.y * math.sin(rad)
-    v.y = v1.x * math.sin(rad) + v1.y * math.cos(rad)
-    return v
+    local x = v1.x * math.cos(rad) - v1.y * math.sin(rad)
+    local y = v1.x * math.sin(rad) + v1.y * math.cos(rad)
+    return Vector.new(x, y)
 end
 
 function Vector.dotProduct(v1, v2)
